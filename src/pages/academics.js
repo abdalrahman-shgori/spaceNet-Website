281c16849cdbdd75b4a467a2ce407a52058bba10
abd <abdalrahman.job9@gmail.com>
2025-04-14 15:03:30 +0300

    update

--- src/pages/academics.js
+++ src/pages/academics.js
@@ -1,65 +1,62 @@
-import React, { Suspense, useEffect, useMemo, useState, lazy } from "react";
-
 import { Grid } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+import SpaceNetLayout from "../components/spaceNetLayout";
 import { motion } from "framer-motion"
-
+import LetsProject from "../components/letsProject";
+import WhichCourse from "../components/academics/whichCourse";
+import SectionDescription from "../components/sectionDescription";
+import LayoutCards from "../components/layoutCards";
 import LifeTime from "../assets/sectionsImages/academics/lifeTime";
 import ClassRoom from "../assets/sectionsImages/academics/classRoom";
 import OnlineCourses from "../assets/sectionsImages/academics/onlineCourses";
 import BootCamp from "../assets/sectionsImages/academics/bootCamp";
+import OurCourses from "../components/academics/ourCourses";
+import DevelopSkillsText from "../components/academics/developSkillsText";
+import DevelopSkills from "../components/academics/developSkills";
+import AcademicsForm from "../components/academics/academicsFrom";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
-import Loader from "../components/loadingPage/loading";
-const SpaceNetLayout = React.lazy(() => import("../components/spaceNetLayout"));
-const LetsProject = lazy(() => import("../components/letsProject"));
-const WhichCourse = lazy(() => import("../components/academics/whichCourse"));
-const SectionDescription = lazy(() => import("../components/sectionDescription"));
-const LayoutCards = lazy(() => import("../components/layoutCards"));
-const OurCourses = lazy(() => import("../components/academics/ourCourses"));
-const DevelopSkillsText = lazy(() => import("../components/academics/developSkillsText"));
-const DevelopSkills = lazy(() => import("../components/academics/developSkills"));
-const AcademicsForm = lazy(() => import("../components/academics/academicsFrom"));
 export default function Academics({ setOpen }) {
     const [hoveredcardid, sethoveredcardid] = useState(null);
     const { t } = useTranslation()
     const technologiesData = useMemo(() => [
         {
-            id: 0,
-            title: t("academics.Coursesin"),
-            description: t("academics.ourCoursesare"),
-            image: [t("academics.English"), t("academics.Kurdish"), t("academics.Arabic")]
+          id: 0,
+          title: t("academics.Coursesin"),
+          description: t("academics.ourCoursesare"),
+          image: [t("academics.English"), t("academics.Kurdish"), t("academics.Arabic")]
         },
         {
-            id: 1,
-            title: t("academics.Lifetime"),
-            description: t("academics.Getlifetime"),
-            image: <LifeTime hoveredcardid={hoveredcardid} itemID={1} />
+          id: 1,
+          title: t("academics.Lifetime"),
+          description: t("academics.Getlifetime"),
+          image: <LifeTime hoveredcardid={hoveredcardid} itemID={1} />
         },
         {
-            id: 2,
-            title: t("academics.anylevel"),
-            description: t("academics.noMatter"),
-            image: [t("academics.Junior"), t("academics.Intermediate"), t("academics.Advanced")]
+          id: 2,
+          title: t("academics.anylevel"),
+          description: t("academics.noMatter"),
+          image: [t("academics.Junior"), t("academics.Intermediate"), t("academics.Advanced")]
         },
         {
-            id: 3,
-            title: t("academics.VirtualClassroom"),
-            description: t("academics.learnFrom"),
-            image: <ClassRoom hoveredcardid={hoveredcardid} itemID={3} />
+          id: 3,
+          title: t("academics.VirtualClassroom"),
+          description: t("academics.learnFrom"),
+          image: <ClassRoom hoveredcardid={hoveredcardid} itemID={3} />
         },
         {
-            id: 4,
-            title: t("academics.pre"),
-            description: t("academics.Ourpremade"),
-            image: <OnlineCourses hoveredcardid={hoveredcardid} itemID={4} />
+          id: 4,
+          title: t("academics.pre"),
+          description: t("academics.Ourpremade"),
+          image: <OnlineCourses hoveredcardid={hoveredcardid} itemID={4} />
         },
         {
-            id: 5,
-            title: t("academics.BootcampCourses"),
-            description: t("academics.ourBootcamp"),
-            image: <BootCamp hoveredcardid={hoveredcardid} itemID={5} />
+          id: 5,
+          title: t("academics.BootcampCourses"),
+          description: t("academics.ourBootcamp"),
+          image: <BootCamp hoveredcardid={hoveredcardid} itemID={5} />
         },
-    ], [t, hoveredcardid]);
+      ], [t, hoveredcardid]);
 
     const [enroll, setEnroll] = useState(false)
 
@@ -97,62 +94,54 @@ export default function Academics({ setOpen }) {
                     duration: 1
                 }}
             >
-                    <AcademicsForm enroll={enroll} setEnroll={setEnroll} />
-                    <Grid className="root-container"
-                        sx={{
-                            padding: {
-                                xl: "100px 116px 0 116px",
-                                lg: "60px 116px 0 116px",
-                                md: "100px 75px 0 75px",
-                                sm: "100px 20px 0 20px",
-                                xs: "100px 20px 0 20px",
-                            },
-                            overflowX: "hidden"
-                        }}
-                    >
-                        <DevelopSkillsText
-                            text={t("academics.developSkills")}
-                        />
-                        <Suspense>
-                        <DevelopSkills setOpen={setOpen} setEnroll={setEnroll} />
-                        </Suspense>
-                        <SectionDescription
-                            text1={t("academics.whatwetech")}
-                            text2={t("academics.exploreOur")}
-                            academicsSection="first"
-
-                        />
-                        <Suspense>
-                        <OurCourses hoveredcardid={hoveredcardid} sethoveredcardid={sethoveredcardid} setEnroll={setEnroll} enroll={enroll} />
-                        </Suspense>
-                        <SectionDescription
-                            text1={t("academics.ourPerks")}
-                            text2={t("academics.FullFeature")}
-                            top="80px"
-                            academicsSection="sec"
-                        />
-                        <Suspense>
-                        <LayoutCards
-                            technologiesData={technologiesData}
-                            sethoveredcardid={sethoveredcardid}
-                            hoveredcardid={hoveredcardid}
-                        />
-</Suspense>
-                    </Grid>
-                    <Suspense>
-                    <WhichCourse />
-                    </Suspense>
-                    <LetsProject
-                        text1={t("academics.Readytokickstart")}
-                        // text2="A SpacenNet technology company can help you improve your idea and turn it into reality if you're facing a challenge"
-                        btnText={t("academics.startNow")}
-                        cardBg="#FA6423"
-                        text1Color="#FFFFFF"
-                        text2Color="#FFFFFF"
-                        setOpen={setOpen}
+                <AcademicsForm enroll={enroll} setEnroll={setEnroll} />
+                <Grid className="root-container"
+                    sx={{
+                        padding: {
+                            xl: "100px 116px 0 116px",
+                            lg: "60px 116px 0 116px",
+                            md: "100px 75px 0 75px",
+                            sm: "100px 20px 0 20px",
+                            xs: "100px 20px 0 20px",
+                        },
+                        overflowX: "hidden"
+                    }}
+                >
+                    <DevelopSkillsText
+                        text={t("academics.developSkills")}
                     />
-                    <SpaceNetLayout setOpen={setOpen} />
+                    <DevelopSkills setOpen={setOpen} setEnroll={setEnroll} />
+                    <SectionDescription
+                        text1={t("academics.whatwetech")}
+                        text2={t("academics.exploreOur")}
+                        academicsSection="first"
 
+                    />
+                    <OurCourses hoveredcardid={hoveredcardid} sethoveredcardid={sethoveredcardid} setEnroll={setEnroll} enroll={enroll} />
+                    <SectionDescription
+                        text1={t("academics.ourPerks")}
+                        text2={t("academics.FullFeature")}
+                        top="80px"
+                        academicsSection="sec"
+                    />
+                    <LayoutCards
+                        technologiesData={technologiesData}
+                        sethoveredcardid={sethoveredcardid}
+                        hoveredcardid={hoveredcardid}
+                    />
+
+                </Grid>
+                <WhichCourse />
+                <LetsProject
+                    text1={t("academics.Readytokickstart")}
+                    // text2="A SpacenNet technology company can help you improve your idea and turn it into reality if you're facing a challenge"
+                    btnText={t("academics.startNow")}
+                    cardBg="#FA6423"
+                    text1Color="#FFFFFF"
+                    text2Color="#FFFFFF"
+                    setOpen={setOpen}
+                />
+                <SpaceNetLayout setOpen={setOpen} />
             </motion.div>
         </>
     )
