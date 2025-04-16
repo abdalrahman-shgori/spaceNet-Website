a570f3b5dd5bb96a29f4ca126f69ab8c155731f3
abd <abdalrahman.job9@gmail.com>
2025-04-16 20:13:33 +0300

    new

--- src/pages/softwarePage.js
+++ src/pages/softwarePage.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { Suspense, useEffect, useMemo, useState } from "react";
 import BusinessGoals from "../components/softwareSection/businessGoals";
 import Grid from "@mui/material/Grid";
-import ContactUsBtn from "../components/softwareSection/contactUsBtn";
 import { useLocation } from "react-router-dom";
-import WhatWeDo from "../components/whatweDo/whatWeDo";
-import SectionDescription from "../components/sectionDescription";
-import LayoutCards from "../components/layoutCards";
 import FrontEndSvg from "../assets/sectionsImages/frontEndImage";
 import BackEndSvg from "../assets/sectionsImages/backendImage";
 import FullStackSvg from "../assets/sectionsImages/fullstack";
@@ -13,12 +9,17 @@ import LowCodeSvg from "../assets/sectionsImages/lowcode";
 import CloudDevelopmentSvg from "../assets/sectionsImages/clouddevelopment";
 import { subServices } from "../services/websiteApis/services";
 import { motion } from "framer-motion"
-import OurTechnologies from "../components/softwareSection/Our technologies Section/ourTechnology";
-import LetsProject from "../components/letsProject";
-import SpaceNetLayout from "../components/spaceNetLayout";
 import { ServiceCategories } from "../services/websiteApis/serviceCategories";
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
+
+const ContactUsBtn = React.lazy(() => import("../components/softwareSection/contactUsBtn"));
+const WhatWeDo = React.lazy(() => import("../components/whatweDo/whatWeDo"));
+const SectionDescription = React.lazy(() => import("../components/sectionDescription"));
+const LayoutCards = React.lazy(() => import("../components/layoutCards"));
+const OurTechnologies = React.lazy(() => import("../components/softwareSection/Our technologies Section/ourTechnology"));
+const LetsProject = React.lazy(() => import("../components/letsProject"));
+const SpaceNetLayout = React.lazy(() => import("../components/spaceNetLayout"));
 
 const serviceColors = {
     "Website Development": "#9D89FC",
@@ -141,6 +142,8 @@ export default function SoftwareSection({ setOpen }) {
                         serviceBgColor="#9D89FC"
                         data={data}
                     />
+                    <Suspense fallback="load">
+
                     <ContactUsBtn setOpen={setOpen} />
                     <WhatWeDo
                         setData={setData}
@@ -164,6 +167,8 @@ export default function SoftwareSection({ setOpen }) {
                         top="43px"
                     />
                     <OurTechnologies />
+                    </Suspense>
+
                 </Grid>
                 <LetsProject
                     text1={<>{t("software.gotGreat")}<br /> {t("software.Ready")} </>}
