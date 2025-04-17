97913f7352bdb9964f3cdf6532d462a559d122be
abd <abdalrahman.job9@gmail.com>
2025-04-17 23:32:36 +0300

    new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import ThemeProvider from './ThemeProvider';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import InnerApp from './pages/LandingPage/innerApp';
 import NavBar from './components/navbar/navbar';
 import { motion } from 'framer-motion';
 import Toggle from './components/toggleCompoent/toggle';
@@ -9,28 +10,22 @@ import LogoAnimation from './pages/LandingPage/logoaniamtion';
 import ThemeSettings from './pagedirection/ThemeSettings';
 import ThemeLocalization from './locals/ThemeLocalization';
 import { useTranslation } from 'react-i18next';
-import SoftwareSection from "./pages/softwarePage"
-import DesignAndBranding from "./pages/design&branding"
-import Academics from "./pages/academics"
-import CoreIt from "./pages/coreIt"
-import BlogsAndNews from './components/blogsAndNews/blogsAndNews';
-import BlogDetails from './components/blogsAndNews/blogDetails';
-import BasicModal from './components/contactUs/contactUs';
-import Footer from './components/footer';
-import InnerApp from './pages/LandingPage/innerApp';
+import Loader from './components/loadingPage/loading';
 
-// const InnerApp = lazy(() => import('./pages/LandingPage/innerApp'))
-// const SoftwareSection = lazy(() => import('./pages/softwarePage'))
-// const Academics = lazy(() => import('./pages/academics'))
-// const CoreIt = lazy(() => import('./pages/coreIt'))
-// const DesignAndBranding = lazy(() => import('./pages/design&branding'))
-// const BlogsAndNews = lazy(() => import('./components/blogsAndNews/blogsAndNews'))
-// const BlogDetails = lazy(() => import('./components/blogsAndNews/blogDetails'))
-// const BasicModal = lazy(() => import('./components/contactUs/contactUs'))
-// const Footer = lazy(() => import('./components/footer'))
+const SoftwareSection = lazy(() => import('./pages/softwarePage'))
+const Academics = lazy(() => import('./pages/academics'))
+const CoreIt = lazy(() => import('./pages/coreIt'))
+const DesignAndBranding = lazy(() => import('./pages/design&branding'))
+const BlogsAndNews = lazy(() => import('./components/blogsAndNews/blogsAndNews'))
+const BlogDetails = lazy(() => import('./components/blogsAndNews/blogDetails'))
+const BasicModal = lazy(() => import('./components/contactUs/contactUs'))
+const Footer = lazy(() => import('./components/footer'))
 
 const App = () => {
-
+  if (process.env.NODE_ENV === 'development') {
+    console.warn = () => { };
+    console.error = () => { };
+  }
   const [open, setOpen] = React.useState(false);
   const [showOverflow, setShowOverFlow] = useState(false)
   const { i18n } = useTranslation();
@@ -72,6 +67,7 @@ const App = () => {
                   background: themeColor,
                 }}
                 exit={{ scaleY: [0, 1.1, 0] }}
+
                 transition={{
                   duration: 0.8,
                   ease: [0.4, 0, 0.2, 1],
@@ -85,6 +81,7 @@ const App = () => {
                   height: "100%",
                   zIndex: 10,
                   transformOrigin: "bottom",
+                  overflow:location.pathname === '/' ? showOverflow ? "auto" : "hidden" : 'unset'
 
                 }}
                 onUpdate={({ height }) => {
@@ -95,34 +92,40 @@ const App = () => {
               >
                 <>
                   <NavBar setOpen={setOpen} showContent={showContent} setDrawerOpen={setDrawerOpen} drawerOpen={drawerOpen} />
+                  <Suspense fallback={<Loader />}>
+                    <Routes>
+                      <Route
+                        path='/'
+                        element={
+                          <InnerApp
+                            showContent={showContent}
+                            setShowContent={setShowContent}
+                            draweOpen={drawerOpen}
+                            setDrawerOpen={setDrawerOpen}
+                            setOpen={setOpen}
+                          />
+                        }
+                      />
+                      <Route path='/software' element={<SoftwareSection setOpen={setOpen} />} />
+                      <Route path='/design-branding' element={<DesignAndBranding setOpen={setOpen} />} />
+                      <Route path='/academics' element={<Academics setOpen={setOpen} />} />
+                      <Route path='/core-it' element={<CoreIt setOpen={setOpen} />} />
+                      <Route path='/blogs' element={<BlogsAndNews setOpen={setOpen} />} />
+                      <Route path="/blogs/:id" element={<BlogDetails setOpen={setOpen} />} />
+                    </Routes>
+                  </Suspense>
 
-                  <Routes>
-                    <Route
-                      path='/'
-                      element={
-                        <InnerApp
-                          showContent={showContent}
-                          setShowContent={setShowContent}
-                          draweOpen={drawerOpen}
-                          setDrawerOpen={setDrawerOpen}
-                          setOpen={setOpen}
-                        />
-                      }
-                    />
-                    <Route path='/software' element={<SoftwareSection setOpen={setOpen} />} />
-                    <Route path='/design-branding' element={<DesignAndBranding setOpen={setOpen} />} />
-                    <Route path='/academics' element={<Academics setOpen={setOpen} />} />
-                    <Route path='/core-it' element={<CoreIt setOpen={setOpen} />} />
-                    <Route path='/blogs' element={<BlogsAndNews setOpen={setOpen} />} />
-                    <Route path="/blogs/:id" element={<BlogDetails setOpen={setOpen} />} />
-                  </Routes>
-
-                  {location.pathname !== '/' && <Footer />}
+                  {location.pathname !== '/' && (
+                    <Suspense fallback={null}>
+                      <Footer />
+                    </Suspense>
+                  )}
                 </>
               </motion.div>
               <Toggle open={open} setThemeColor={setThemeColor} themeColor={themeColor} drawerOpen={drawerOpen} />
-
-              <BasicModal setOpen={setOpen} open={open} />
+              <Suspense fallback={null}>
+                <BasicModal setOpen={setOpen} open={open} />
+              </Suspense>
             </>
           )}
         </ThemeLocalization>
