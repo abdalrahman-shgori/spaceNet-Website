97b69afc615facbfa2203a5640308a94d6eb7f9f
abd <abdalrahman.job9@gmail.com>
2025-04-21 15:30:24 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
 
-import React, { lazy, Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ThemeProvider from './ThemeProvider';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import NavBar from './components/navbar/navbar';
 import { motion } from 'framer-motion';
 import Toggle from './components/toggleCompoent/toggle';
+import LogoAnimation from './pages/LandingPage/logoaniamtion';
 import ThemeSettings from './pagedirection/ThemeSettings';
 import ThemeLocalization from './locals/ThemeLocalization';
 import { useTranslation } from 'react-i18next';
-
-const LogoAnimation = lazy(() => import('./pages/LandingPage/logoaniamtion'));
-const SoftwareSection = lazy(() => import('./pages/softwarePage'));
-const DesignAndBranding = lazy(() => import('./pages/design&branding'));
-const Academics = lazy(() => import('./pages/academics'));
-const CoreIt = lazy(() => import('./pages/coreIt'));
-const BlogsAndNews = lazy(() => import('./components/blogsAndNews/blogsAndNews'));
-const BlogDetails = lazy(() => import('./components/blogsAndNews/blogDetails'));
-const BasicModal = lazy(() => import('./components/contactUs/contactUs'));
-const Footer = lazy(() => import('./components/footer'));
-const InnerApp = lazy(() => import('./pages/LandingPage/innerApp'));
+import SoftwareSection from "./pages/softwarePage"
+import DesignAndBranding from "./pages/design&branding"
+import Academics from "./pages/academics"
+import CoreIt from "./pages/coreIt"
+import BlogsAndNews from './components/blogsAndNews/blogsAndNews';
+import BlogDetails from './components/blogsAndNews/blogDetails';
+import BasicModal from './components/contactUs/contactUs';
+import Footer from './components/footer';
+import InnerApp from './pages/LandingPage/innerApp';
+// const InnerApp = lazy(() => import('./pages/LandingPage/innerApp'))
+// const SoftwareSection = lazy(() => import('./pages/softwarePage'))
+// const Academics = lazy(() => import('./pages/academics'))
+// const CoreIt = lazy(() => import('./pages/coreIt'))
+// const DesignAndBranding = lazy(() => import('./pages/design&branding'))
+// const BlogsAndNews = lazy(() => import('./components/blogsAndNews/blogsAndNews'))
+// const BlogDetails = lazy(() => import('./components/blogsAndNews/blogDetails'))
+// const BasicModal = lazy(() => import('./components/contactUs/contactUs'))
+// const Footer = lazy(() => import('./components/footer'))
 
 const App = () => {
  
   const [open, setOpen] = React.useState(false);
   const [showOverflow, setShowOverFlow] = useState(false)
   const { i18n } = useTranslation();
+  document.body.dir = i18n.dir();
   const location = useLocation();
   const [drawerOpen, setDrawerOpen] = useState(false)
   const [showContent, setShowContent] = useState(false);
   const [logoAnimationComplete, setLogoAnimationComplete] = useState(false);
   const [themeColor, setThemeColor] = useState('')
-  useEffect(() => {
-    document.body.dir = i18n.dir();
-  }, [i18n]);
   const handleAnimationComplete = () => {
     setLogoAnimationComplete(true);
   };
   useEffect(() => {
-    if (location.pathname !== '/') {
-      setLogoAnimationComplete(true);
-    }
-  }, [location.pathname]);
-  
-  useEffect(() => {
     if (logoAnimationComplete) {
-      requestIdleCallback(() => setShowContent(true));
+      setTimeout(() => {
+        setShowContent(true);
+      }, 0);
+    }
+    else if (location.pathname !== '/') {
+      setLogoAnimationComplete(true)
     }
   }, [logoAnimationComplete]);
-  
-  
 
   return (
     <ThemeProvider logoAnimationComplete={logoAnimationComplete}>
       {location.pathname === '/' && (
-        <Suspense fallback={null}>
         <LogoAnimation handleAnimationComplete={handleAnimationComplete} logoAnimationComplete={logoAnimationComplete} />
-        </Suspense>
       )}
 
       <ThemeSettings>
@@ -69,9 +70,9 @@ const App = () => {
                   background: themeColor,
                 }}
                 exit={{ scaleY: [0, 1.1, 0] }}
+
                 transition={{
-                  delay: 0.2,
-                  duration: 0.6,
+                  duration: 0.8,
                   ease: [0.4, 0, 0.2, 1],
                   times: [0, 0.5, 1],
                 }}
@@ -85,13 +86,14 @@ const App = () => {
                   transformOrigin: "bottom",
                   overflow: location.pathname === '/' ? showOverflow ? "auto" : "hidden" : 'unset'
                 }}
-                onAnimationComplete={()=>{
-                  setShowOverFlow(true)
+                onUpdate={({ height }) => {
+                  if (height === '100dvh') {
+                    setShowOverFlow(true)
+                  }
                 }}
               >
                 <>
                   <NavBar setOpen={setOpen} showContent={showContent} setDrawerOpen={setDrawerOpen} drawerOpen={drawerOpen} />
-                  <Suspense fallback={<div>Loading...</div>}>
                   <Routes>
                     <Route
                       path='/'
@@ -112,25 +114,13 @@ const App = () => {
                     <Route path='/blogs' element={<BlogsAndNews setOpen={setOpen} />} />
                     <Route path="/blogs/:id" element={<BlogDetails setOpen={setOpen} />} />
                   </Routes>
-                  </Suspense>
-
-                
                   {location.pathname !== '/' && (
-                    <Suspense fallback={null}>
                     <Footer />
-                    </Suspense>
                   )}
                 </>
               </motion.div>
-              {showContent && (
-                <>
-                <Suspense fallback={null}>
-                <Toggle open={open} setThemeColor={setThemeColor} themeColor={themeColor} drawerOpen={drawerOpen} />
-                <BasicModal setOpen={setOpen} open={open} />
-                </Suspense>
-                </>
-              )}
-             
+              <Toggle open={open} setThemeColor={setThemeColor} themeColor={themeColor} drawerOpen={drawerOpen} />
+              <BasicModal setOpen={setOpen} open={open} />
             </>
           )}
         </ThemeLocalization>
